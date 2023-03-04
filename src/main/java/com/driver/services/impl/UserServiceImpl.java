fe542bb304f22500588db25fa10d96f25d49b2ca
Rishabh <rishabhaj13@gmail.com>
2023-03-04 19:58:21 +0530

    completed user register and subscribe

--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.driver.services.impl;
 
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
 import com.driver.model.User;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
@@ -21,14 +24,62 @@ public class UserServiceImpl implements UserService {
     @Override
     public User register(String username, String password, String countryName) throws Exception{
         User user = new User(username,password);
+        Country country = new Country();
+
+        if(!isValidCountryName(countryName)){
+            throw new Exception("Country not found");
+        }
+
+        if (countryName.equalsIgnoreCase("IND")){
+            country.setCountryName(CountryName.IND);
+            country.setCode(CountryName.IND.toCode());
+        }
+        if (countryName.equalsIgnoreCase("USA")){
+            country.setCountryName(CountryName.USA);
+            country.setCode(CountryName.USA.toCode());
+        }
+        if (countryName.equalsIgnoreCase("AUS")){
+            country.setCountryName(CountryName.AUS);
+            country.setCode(CountryName.AUS.toCode());
+        }
+        if (countryName.equalsIgnoreCase("JPN")){
+            country.setCountryName(CountryName.JPN);
+            country.setCode(CountryName.JPN.toCode());
+        }
+        if (countryName.equalsIgnoreCase("CHI")){
+            country.setCountryName(CountryName.CHI);
+            country.setCode(CountryName.CHI.toCode());
+        }
+
+        country.setUser(user);
+        user.setOriginalCountry(country);
+        user.setConnected(false);
+        userRepository3.save(user);
+
+        String IP = country.getCode()+"."+user.getId();
+        user.setOriginalIp(IP);
+
         userRepository3.save(user);
         return user;
+    }
+
+    private boolean isValidCountryName(String countryName){
+        if(countryName.equalsIgnoreCase("IND") || countryName.equalsIgnoreCase("USA") || countryName.equalsIgnoreCase("AUS")||countryName.equalsIgnoreCase("JPN")||countryName.equalsIgnoreCase("CHI")) {
+            return true;
+        }
+        else
+            return false;
     }
 
     @Override
     public User subscribe(Integer userId, Integer serviceProviderId) {
         User user = userRepository3.findById(userId).get();
+        ServiceProvider serviceProvider = serviceProviderRepository3.findById(serviceProviderId).get();
 
+        user.getServiceProviderList().add(serviceProvider);
+        serviceProvider.getUsers().add(user);
+
+        serviceProviderRepository3.save(serviceProvider);
         return user;
     }
 }
